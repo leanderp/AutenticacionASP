ca7e90de648ef8b2b8eee4f678935cd16187c0f3
Leanderp <leanderperez15@gmail.com>
2021-02-06 10:11:26 -0400

    prueba

---
+++ AutenticacionASP/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutenticacionASP.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(120)]
+        public string LugarDeNacimiento { get; set; }
+    }
+}
